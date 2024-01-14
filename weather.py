74a8ce0a1a5756cfd42d0ff76e9a4bbeb06be8c9
Mike Heavers <mheavers@gmail.com>
2024-01-14 10:56:21 -0800

    rename, pass coords programmatically

--- weather.py
+++ weather.py
@@ -8,10 +8,11 @@ load_dotenv()
 TOMORROW_KEY = os.environ.get("TOMORROW_KEY")
 TIMESTEP = os.environ.get("TIMESTEP")
 UNITS= os.environ.get("UNITS")
+COORDS= os.environ.get("COORDS")
 
-def get_weather_data(lat,lon,apiKey,timestep,units):
-    URL = Template('https://api.tomorrow.io/v4/weather/forecast?location=$lat,$lon&apikey=$apiKey&fields=temperature&timesteps=$timestep&units=$units')
-    r = requests.get(url = URL.substitute(lat=lat,lon=lon,apiKey=apiKey,timestep=timestep,units=units))
+def get_weather_data(coords,apiKey,timestep,units):
+    URL = Template('https://api.tomorrow.io/v4/weather/forecast?location=$coords&apikey=$apiKey&fields=temperature&timesteps=$timestep&units=$units')
+    r = requests.get(url = URL.substitute(coords=coords,apiKey=apiKey,timestep=timestep,units=units))
     data = r.json()
     # return data['data']['timelines'][0]['intervals'][0]
     # return data.timelines.minutely[0].values.temperature
@@ -20,5 +21,5 @@ def get_weather_data(lat,lon,apiKey,timestep,units):
 
 
 if __name__ == "__main__":
-    weather = get_weather_data(45.6427,25.5887,TOMORROW_KEY,TIMESTEP, UNITS)
+    weather = get_weather_data(COORDS,TOMORROW_KEY,TIMESTEP, UNITS, COORDS)
     print(weather)
